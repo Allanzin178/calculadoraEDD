d3938ac2587e9516b8bce0ad85b1bdef44ee5d96
Allanzin178 <allan.miron@hotmail.com>
2024-11-20 19:13:00 -0300

    Forma final

--- expressao.c
+++ expressao.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h> // Para isdigit
 #include <math.h>
 #include "expressao.h"
@@ -11,10 +12,14 @@
 #define TG 1
 #define SEN 2
 #define LOG 2
+#define PI 3.14159265358979323846
+
+//---------------- STRUCT ----------------
 
 typedef struct Item
 {
-    float Chave;
+    char expressao[512];
+    double Chave;
     struct Item *Anterior;
 } Item;
 
@@ -24,11 +29,17 @@ typedef struct
     Item *Topo;
 } Pilha;
 
+//---------------- FUNÇÕES ADICIONAIS ----------------
+
 Pilha *inicializar();
 void empilhar(Pilha *P, Item *I);
 Item *desempilhar(Pilha *P);
-Item *criarItem(float C);
+Item *criarItem(double C);
 Item *variosNum(Pilha *P, int cont, int contVirg);
+void empilharString(Pilha *P, char *exp);
+void liberarPilha(Pilha *P);
+
+//---------------- ESTUTURANDO FUNÇÕES ----------------
 
 Pilha *inicializar()
 {
@@ -51,6 +62,7 @@ void empilhar(Pilha *P, Item *I)
     I->Anterior = P->Topo;
     P->Topo = I;
     P->Tamanho++;
+    // printf("Empilhando %.1f:\n", I->Chave);
 }
 
 Item *desempilhar(Pilha *P)
@@ -64,11 +76,21 @@ Item *desempilhar(Pilha *P)
     Item *I = P->Topo;
     P->Topo = I->Anterior;
     P->Tamanho--;
+    // printf("Desempilhando %.1f:\n", I->Chave);
 
     return I;
 }
 
-Item *criarItem(float C)
+void empilharString(Pilha *P, char *exp)
+{
+    Item *novo = criarItem(0);
+    strcpy(novo->expressao, exp);
+    novo->Anterior = P->Topo;
+    P->Topo = novo;
+    P->Tamanho++;
+}
+
+Item *criarItem(double C)
 {
     Item *E = (Item *)malloc(sizeof(Item));
 
@@ -78,31 +100,57 @@ Item *criarItem(float C)
         exit(1);
     }
     E->Chave = C;
+    strcpy(E->expressao, "");
     E->Anterior = NULL;
 
     return E;
+}
+
+void liberarPilha(Pilha *P)
+{
+    if (P == NULL)
+    {
+        printf("Erro! Pilha vazia");
+        exit(1);
+    }
+    while (P->Topo != NULL)
+    {
+        Item *I = desempilhar(P);
+        free(I);
+    }
+
+    free(P);
 }
 // --------------------------- fundao ----------------------------------------
 
 Item *variosNum(Pilha *P, int cont, int contVirg)
 {
-    float soma = 0;
+    double soma = 0;
     // IF VIRGULA > 0
-    if(contVirg > 0){
+    if (contVirg > 0)
+    {
         // FOR VIRGULA
-        for(int i = contVirg; i > 0; i--){
-            soma = soma + (desempilhar(P)->Chave / pow(10, i));
-            printf("%.3f\n", soma);
+        for (int i = contVirg; i > 0; i--)
+        {
+            Item *I = desempilhar(P);
+            soma = soma + (I->Chave / pow(10, i));
+            free(I);
+            // printf("%f\n", soma);
         }
     }
-    
+
+    // printf("\tApós a virgula: \n");
     // FOR NAO VIRGULA
     for (int i = 0; i < cont; i++)
     {
-        printf("Teste %d %d %d\n", i, cont, contVirg);
-        soma = soma + (desempilhar(P)->Chave * pow(10, i));
-        printf("%.3f\n", soma);
+        Item *I = desempilhar(P);
+        // printf("%f * %.2f = ", I->Chave, pow(10, i));
+        soma = soma + (I->Chave * pow(10, i));
+        free(I);
+        // printf("%f\n", soma);
     }
+
+    // printf("%f\n", soma);
     Item *I = criarItem(soma);
     return I;
 }
@@ -113,12 +161,17 @@ float getValor(char *Expressao)
 {
     Pilha *P = inicializar();
     int cont = 0, contVirg = 0;
-    int virgula = NAO;
+    int virgula = NAO, negativo = NAO;
 
     for (int i = 0; Expressao[i] != '\0'; i++)
     {
-        if (isdigit(Expressao[i]))
+        if (isdigit(Expressao[i]) || (Expressao[i] == '-' && isdigit(Expressao[i + 1])))
         {
+            if (Expressao[i] == '-')
+            {
+                negativo = SIM;
+                continue;
+            }
             if (virgula == NAO)
             {
                 cont++;
@@ -127,8 +180,9 @@ float getValor(char *Expressao)
             {
                 contVirg++;
             }
-            Item *I = criarItem((float)(Expressao[i] - '0'));
+            Item *I = criarItem((double)(Expressao[i] - '0'));
             empilhar(P, I);
+            
         }
         else if (Expressao[i] == '.')
         {
@@ -143,6 +197,13 @@ float getValor(char *Expressao)
             {
                 empilhar(P, variosNum(P, cont, contVirg));
             }
+            if (negativo == SIM)
+            {
+                Item *I1 = desempilhar(P);
+                I1->Chave = I1->Chave * -1;
+                empilhar(P, I1);
+                negativo = NAO;
+            }
             cont = 0;
             contVirg = 0;
             virgula = NAO;
@@ -151,16 +212,23 @@ float getValor(char *Expressao)
         else
         {
 
-            float Operando1;
-            float Operando2;
-
-            float Res;
+            double Operando1;
+            double Operando2;
+            double radianos;
+            double Res;
             Item *I;
 
             // NAO MUDA ISSO!!!!!
             if (cont > 1 || contVirg > 0)
             {
                 empilhar(P, variosNum(P, cont, contVirg));
+            }
+            if (negativo == SIM)
+            {
+                Item *I1 = desempilhar(P);
+                I1->Chave = I1->Chave * -1;
+                empilhar(P, I1);
+                negativo = NAO;
             }
             switch (Expressao[i])
             {
@@ -169,19 +237,19 @@ float getValor(char *Expressao)
                 Operando2 = desempilhar(P)->Chave;
                 Operando1 = desempilhar(P)->Chave;
                 Res = Operando1 + Operando2;
-                printf("%.1f %.1f %.1f\n", Operando1, Operando2, Res);
+                // printf("%.1f %.1f %.1f\n", Operando1, Operando2, Res);
                 break;
             case '-':
                 Operando2 = desempilhar(P)->Chave;
                 Operando1 = desempilhar(P)->Chave;
                 Res = Operando1 - Operando2;
-                printf("%.1f %.1f %.1f\n", Operando1, Operando2, Res);
+                // printf("%.1f %.1f %.1f\n", Operando1, Operando2, Res);
                 break;
             case '*':
                 Operando2 = desempilhar(P)->Chave;
                 Operando1 = desempilhar(P)->Chave;
                 Res = Operando1 * Operando2;
-                printf("%.1f %.1f %.1f\n", Operando1, Operando2, Res);
+                // printf("%.1f %.1f %.1f\n", Operando1, Operando2, Res);
                 break;
             case '/':
                 Operando2 = desempilhar(P)->Chave;
@@ -189,16 +257,17 @@ float getValor(char *Expressao)
                 if (Operando2 == 0)
                 {
                     printf("ERRO: Divisão por zero!\n");
-                    exit(EXIT_FAILURE);
+                    liberarPilha(P);
+                    return 0;
                 }
                 Res = Operando1 / Operando2;
-                printf("%.1f %.1f %.1f\n", Operando1, Operando2, Res);
+                // printf("%.1f %.1f %.1f\n", Operando1, Operando2, Res);
                 break;
             case '^':
                 Operando2 = desempilhar(P)->Chave;
                 Operando1 = desempilhar(P)->Chave;
                 Res = pow(Operando1, Operando2);
-                printf("%.1f\n", Res);
+                // printf("%.1f %.1f %.1f\n", Operando1, Operando2, Res);
                 break;
 
             // ----------FUNÇAO DE LOG----------
@@ -208,12 +277,20 @@ float getValor(char *Expressao)
                 if (Expressao[i + 1] != 'o' || Expressao[i + 2] != 'g')
                 {
                     printf("ERRO");
-                    exit(EXIT_FAILURE);
+                    liberarPilha(P);
+                    return 0;
+                }
+
+                if (Operando1 <= 0)
+                {
+                    printf("ERRO! Logaritmo invalido");
+                    liberarPilha(P);
+                    return 0;
                 }
 
                 Res = log10(Operando1);
                 i += LOG;
-                printf("%.1f %.1f\n", Operando1, Res);
+                // printf("%.1f\n", Res);
                 break;
             // ----------FUNÇAO DE RAIZ----------
             case 'r':
@@ -222,58 +299,80 @@ float getValor(char *Expressao)
                 if (Expressao[i + 1] != 'a' || Expressao[i + 2] != 'i' || Expressao[i + 3] != 'z')
                 {
                     printf("ERRO");
-                    exit(EXIT_FAILURE);
+                    liberarPilha(P);
+                    return 0;
+                }
+
+                if (Operando1 < 0)
+                {
+                    printf("Raiz de números negativos são invalidos\n");
+                    liberarPilha(P);
+                    return 0;
                 }
 
                 Res = sqrt(Operando1);
                 i += RAIZ;
-                printf("%.1f\n", Res);
+                // printf("%.1f\n", Res);
                 break;
             // ----------FUNÇAO SENO---------
             case 's':
                 Operando1 = desempilhar(P)->Chave;
+                radianos = Operando1 * (PI / 180.0);
 
                 if (Expressao[i + 1] != 'e' || Expressao[i + 2] != 'n')
                 {
                     printf("ERRO");
-                    exit(EXIT_FAILURE);
+                    liberarPilha(P);
+                    return 0;
                 }
 
-                Res = sin(Operando1);
+                Res = sin(radianos);
                 i += SEN;
-                printf("%.1f\n", Res);
+                // printf("%.1f\n", Res);
                 break;
             // ----------FUNÇAO COSSENO---------
             case 'c':
                 Operando1 = desempilhar(P)->Chave;
+                radianos = Operando1 * (PI / 180.0);
 
                 if (Expressao[i + 1] != 'o' || Expressao[i + 2] != 's')
                 {
                     printf("ERRO");
-                    exit(EXIT_FAILURE);
+                    liberarPilha(P);
+                    return 0;
                 }
-
-                Res = cos(Operando1);
+                Res = cos(radianos);
                 i += COS;
-                printf("%.1f\n", Res);
+                // printf("%.1f\n", Res);
                 break;
             // ----------FUNÇAO TANGENTE---------
             case 't':
                 Operando1 = desempilhar(P)->Chave;
+                radianos = Operando1 * (PI / 180.0);
 
                 if (Expressao[i + 1] != 'g')
                 {
                     printf("ERRO");
-                    exit(EXIT_FAILURE);
+                    liberarPilha(P);
+                    return 0;
                 }
 
-                Res = tan(Operando1);
+                if (fabs(cos(radianos)) < 1e-10)
+                {
+                    printf("ERRO! IMPOSSIVEL FAZER O CALCULO DA TANGENTE!\n");
+                    liberarPilha(P);
+                    return 0;
+                }
+
+                Res = tan(radianos);
                 i += TG;
-                printf("%.1f\n", Res);
+                // printf("%.1f\n", Res);
                 break;
+
             default:
                 printf("ERRO: operador invalido!\n");
-                exit(EXIT_FAILURE);
+                liberarPilha(P);
+                return 0;
                 break;
             }
 
@@ -285,10 +384,108 @@ float getValor(char *Expressao)
         }
     }
 
-    Item *resultadoItem = desempilhar(P);
-    float resultado = resultadoItem->Chave;
-    free(resultadoItem); // Liberar a memória do resultado
-    free(P);             // Liberar a memória da pilha
+    if (negativo == SIM)
+    {
+        Item *I1 = desempilhar(P);
+        I1->Chave = I1->Chave * -1;
+        empilhar(P, I1);
+        negativo = NAO;
+    }
 
+    Item *resultadoItem = desempilhar(P);
+    double resultado = resultadoItem->Chave;
+    free(resultadoItem); // Liberar a memória do resultado
+    liberarPilha(P);
+
+    return resultado;
+}
+
+char *getFormaInFixa(char *Expressao)
+{
+    Pilha *P = inicializar();
+    char token[512];
+    int i = 0;
+
+    while (Expressao[i] != '\0')
+    {
+        if (isdigit(Expressao[i]) || (Expressao[i] == '-' && (i == 0 || Expressao[i - 1] == ' ' && isdigit(Expressao[i + 1]))))
+        { // Número ou número negativo
+            int j = 0;
+            if (Expressao[i] == '-')
+            {
+                token[j++] = Expressao[i++]; // Adicionar o sinal de negativo
+            }
+            while (isdigit(Expressao[i]) || Expressao[i] == '.')
+            {
+                token[j++] = Expressao[i++];
+            }
+            token[j] = '\0';
+            empilharString(P, token);
+        }
+        else if (Expressao[i] == ' ' || Expressao[i] == '\t')
+        {
+            i++; // Ignorar espaços
+        }
+        else if (strncmp(&Expressao[i], "sen", 3) == 0)
+        { // Seno
+            i += 3;
+            char *arg = desempilhar(P)->expressao;
+            char novaExpressao[512];
+            sprintf(novaExpressao, "sen(%s)", arg);
+            free(arg);
+            empilharString(P, novaExpressao);
+        }
+        else if (strncmp(&Expressao[i], "cos", 3) == 0)
+        { // Cosseno
+            i += 3;
+            char *arg = desempilhar(P)->expressao;
+            char novaExpressao[512];
+            sprintf(novaExpressao, "cos(%s)", arg);
+            free(arg);
+            empilharString(P, novaExpressao);
+        }
+        else if (strncmp(&Expressao[i], "tg", 2) == 0)
+        { // Tangente
+            i += 2;
+            char *arg = desempilhar(P)->expressao;
+            char novaExpressao[512];
+            sprintf(novaExpressao, "tg(%s)", arg);
+            free(arg);
+            empilharString(P, novaExpressao);
+        }
+        else if (strncmp(&Expressao[i], "log", 3) == 0)
+        { // Logaritmo
+            i += 3;
+            char *arg = desempilhar(P)->expressao;
+            char novaExpressao[512];
+            sprintf(novaExpressao, "log(%s)", arg);
+            free(arg);
+            empilharString(P, novaExpressao);
+        }
+        else if (strncmp(&Expressao[i], "raiz", 4) == 0)
+        { // Raiz quadrada
+            i += 4;
+            char *arg = desempilhar(P)->expressao;
+            char novaExpressao[512];
+            sprintf(novaExpressao, "raiz(%s)", arg);
+            free(arg);
+            empilharString(P, novaExpressao);
+        }
+        else
+        { // Operadores (como +, -, *, /, ^)
+            char operador = Expressao[i];
+            char *direito = desempilhar(P)->expressao;
+            char *esquerdo = desempilhar(P)->expressao;
+            char novaExpressao[512];
+            sprintf(novaExpressao, "(%s %c %s)", esquerdo, operador, direito);
+            free(direito);
+            free(esquerdo);
+            empilharString(P, novaExpressao);
+            i++;
+        }
+    }
+
+    char *resultado = desempilhar(P)->expressao;
+    liberarPilha(P);
     return resultado;
 }
--- main.c
+++ main.c
@@ -1,43 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "expressao.h"
 
 int main(void) {
-    char *Expressao = "225.25 225.25 + 450.25 +";
-    float resultado = getValor(Expressao);
-    printf("Resultado 1: %f \n", resultado);
 
-    char *Expressao1 = "7 2 * 4 + ";
-    resultado = getValor(Expressao1);
-    printf("Resultado 2: %f \n", resultado);
-
+    int numTestes = 9;
+    Expressao E[numTestes];
+    system("cls");
+    
+    char *Expressao = "3 4 + 5 * ";
+    char *Expressao1 = "7 2 * 4 +";
     char *Expressao2 = "8 5 2 4 + * +";
-    resultado = getValor(Expressao2);
-    printf("Resultado 2: %f \n", resultado);
-
-    char *Expressao3 = "6 2 / 3 + 4 *";
-    resultado = getValor(Expressao3);
-    printf("Resultado 2: %f \n", resultado);
-
+    char *Expressao3 = "6 2 / 3 + 4 * ";
     char *Expressao4 = "9 5 2 8 * 4 + * +";
-    resultado = getValor(Expressao4);
-    printf("Resultado 2: %f \n", resultado);
-
     char *Expressao5 = "2 3 + log 5 / ";
-    resultado = getValor(Expressao5);
-    printf("Resultado 2: %f \n", resultado);
-
     char *Expressao6 = "10 log 3 ^ 2 +";
-    resultado = getValor(Expressao6);
-    printf("Resultado 2: %f \n", resultado);
-
     char *Expressao7 = "45 60 + 30 cos *";
-    resultado = getValor(Expressao7);
-    printf("Resultado 2: %f \n", resultado);
-
     char *Expressao8 = "0.5 45 sen 2 ^ +";
-    resultado = getValor(Expressao8);
-    printf("Resultado 2: %f \n", resultado);
+    // char *Expressao9 = "450 tg";
+    // char *Expressao10 = "64 raiz";
+
+    strcpy(E[0].posFixa, Expressao);
+    strcpy(E[1].posFixa, Expressao1);
+    strcpy(E[2].posFixa, Expressao2);
+    strcpy(E[3].posFixa, Expressao3);
+    strcpy(E[4].posFixa, Expressao4);
+    strcpy(E[5].posFixa, Expressao5);
+    strcpy(E[6].posFixa, Expressao6);
+    strcpy(E[7].posFixa, Expressao7);
+    strcpy(E[8].posFixa, Expressao8);
+    // strcpy(E[9].posFixa, Expressao9);
+    // strcpy(E[10].posFixa, Expressao10);
+
+    for(int i = 0; i < numTestes; i++){
+        strcpy(E[i].inFixa, getFormaInFixa(E[i].posFixa));
+
+        printf("\n\t TESTE %d: \n", i + 1);
+
+        E[i].Valor = getValor(E[i].posFixa);
+        printf("Resultado: %.2f\n", E[i].Valor);
+        printf("Pos-fixa: %s\n", E[i].posFixa);
+        printf("Infixa: %s\n", E[i].inFixa);
+    }
+    
+    
+    
 
     return 0;
 }
